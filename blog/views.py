70001b76510602cfdba5d5f71b9852b12df998ac
djahnie <djahnie@disroot.org>
2017-11-18 18:04:01 +0100

    add post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotFound
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    bla = render(request, 'blog/post_list.html', {'posts': posts})
-    print(bla)
-    return bla
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return HttpResponseNotFound('<h1>Sorry, no such blog post here... :(</h1>')
+    return render(request, 'blog/post_detail.html', {'post': post})
